8f868df045fdbe3abee8046ce0e454abba399f33
mulloa90 <marioulloa@Marios-iMac.attlocal.net>
2022-07-28 16:25:08 -0700

    updated lat and lon

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // openstreetmap API
+var API_KEY = '533d85454b838c8602df5b7476173c44';
+var searchButton = document.querySelector("searchButton");
+var searchForm = document.querySelector("#searchForm")
+
+searchForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    getLatandLon(e.target.children[1].value)
+    
+})
+
+function getLatandLon(cityName) {
+    fetch(
+      `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`
+    )
+      .then((result) => {
+        return result.json();
+      })
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
