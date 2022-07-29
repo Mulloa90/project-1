87e10ea5205c80f565c052862505d6e256073e0d
aurumbright <aurum.elean@gmail.com>
2022-07-28 21:50:53 -0700

    hooked up overpass api

--- map.js
+++ map.js
@@ -1,5 +1,10 @@
 // Building the map and centering it on San Diego - this uses Leaflet, an open-source JS library and tiles from OpenStreetMaps
-var map = L.map('map').setView([32.7157, -117.1611], 12);
+
+let map = L.map('map').setView([32.7157, -117.1611], 12);
+
+let hotels = document.getElementById('hotels');
+let artworks = document.getElementById('artworks');
+let museums = document.getElementById('museums');
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -18,7 +23,6 @@ let formSubmitHandler = function (event) {
 
     if (cityName) {
         relocate(cityName);
-
     } else {
         alert('Please enter a city name');
     }
@@ -43,9 +47,68 @@ let relocate = function (city) {
 
                     map.panTo([latitude, longitude]);
 
+                    let north = map.getBounds().getNorth();
+                    let east = map.getBounds().getEast();
+                    let south = map.getBounds().getSouth();
+                    let west = map.getBounds().getWest();
+
+                    let bbox = south + ',' + west + ',' + north + ',' + east;
+                    console.log(bbox);
+
+                    if (museums) {
+                    fetch(
+
+                        'https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];(node["tourism"="museum"](' + bbox + ');way["tourism"="museum"](' + bbox + ');relation["tourism"="museum"](' + bbox + '););out;>;out skel qt;'
+                    )
+                        .then((result) => {
+                            return result.json();
+                        })
+                        .then((data) => {
+                            console.log(data);
+                        })
+                    }
+
                 })
             }
         })
 };
 
+
+// object keys: lat lon tags.name
+
+/*
+
+markers = [
+   {
+     "name": tags.name,
+     "lat": lat,
+     "lng": lon
+   },
+
+for (var i=0; i < markers.length; ++i) 
+{
+   L.marker([markers[i].lat, markers[i].lng])
+      .bindPopup('<a href="" target="_blank" rel="noopener">' + markers[i].name + '</a>')
+      .addTo(map);
+}
+
+    let cityURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + weatherAPIKey;
+    let latLonData;
+
+    fetch(cityURL)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                    latLonData = data;
+                    console.log(latLonData);
+
+                    let latitude = data[0].lat;
+                    let longitude = data[0].lon;
+
+*/
+
+
 searchFormEl.addEventListener("click", formSubmitHandler);
+
